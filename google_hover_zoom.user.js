53e8859f867a20dc875b89e7b6265a397207a95d
Tommy Chen <tommy351@gmail.com>
2012-05-12 11:38:59 +0800

    use array instead of $.data to save data

--- google_hover_zoom.user.js
+++ google_hover_zoom.user.js
@@ -1680,14 +1680,16 @@ var process = {
 		var target = $(obj).children(selectors[6]),
 			length = target.length,
 			parentWidth = $(obj).width(),
+			original = [],
+			modified = [],
 			button = $('<div class="hz_dlButton hz_maxPicResize" aria-label="'+lang.maxpic01+'" data-tooltip="'+lang.maxpic01+'"><span></span></div>').click(function(){
 				if (target.eq(0).hasClass('hz_maxPic_container')){
-					target.each(function(){
-						$(this).removeClass('hz_maxPic_container').children().children().attr('src', $(this).data('original'));
+					target.each(function(i){
+						$(this).removeClass('hz_maxPic_container').children().children().attr('src', original[i]);
 					});
 				} else {
-					target.each(function(){
-						$(this).addClass('hz_maxPic_container').children().children().attr('src', $(this).data('modified'));
+					target.each(function(i){
+						$(this).addClass('hz_maxPic_container').children().children().attr('src', modified[i]);
 					});
 				}
 
@@ -1697,11 +1699,12 @@ var process = {
 		target.each(function(i){
 			var children = this.childNodes[0].childNodes[0],
 				src = children.src;
-			$(this).data('original', src);
+			original.push(src);
 			src = length == 3 && i > 0 && options.hz_maxpic_option === '1' ? src.replace(picasaRegex, '/w'+parseInt(parentWidth/2)+'-h'+parseInt(parentWidth/2)+'-p/$2') : src.replace(picasaRegex, '/w'+parentWidth+'/$2');
 			
 			children.src = src;
-			$(this).addClass('hz_maxPic_container').data('modified', src);
+			modified.push(src);
+			$(this).addClass('hz_maxPic_container');
 		});
 
 		$(obj).parentsUntil(selectors[4]).next().children().eq(-1).before(button);
